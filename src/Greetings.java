3bb74256490bd502b062c7119efa6b1fe27b7815
srodante <srodante@gmail.com>
2021-10-06 13:55:08 -0400

    Greetings

---
+++ src/Greetings.java
@@ -0,0 +1,9 @@
+public class Greetings {
+    static void greeting(int x){
+        System.out.println("Hello " + x);
+    }
+    public static void main (String[] args){
+        greeting(3);
+    }
+}
+
